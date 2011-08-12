a3a05eae86141d075cbbbb81bd4f78ba87387af4
Luke Semerau <luke.semerau@gmail.com>
2011-08-12 05:02:05 +0000

    LukeIS: updating explicitly wait python example in Chapter4 with new WebDriverWait

--- src/main/rst/examples/Chapter4/python/ExplicitWaitExample01.py
+++ src/main/rst/examples/Chapter4/python/ExplicitWaitExample01.py
@@ -1,33 +1,9 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-import time
-
-class WebDriverWait:
-    def __init__(self, driver, timeout):
-        self._driver = driver
-        self._timeout = timeout
-        if timeout == 0:
-        	self._timeout = 0.5
-
-    def until(self, method):
-        for i in xrange(int(2 * self._timeout)):
-            try:
-                value = method(self._driver)
-                print value
-                if value != False:
-                    return value
-            except NoSuchElementException, nse:
-                pass
-            time.sleep(0.5)
-        raise TimeoutException()
- 
-def until(driver):
-    return driver.find_element_by_id("myDynamicElement")
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 
 ff = webdriver.Firefox()
 ff.get("http://somedomain/url_that_delays_loading")
 try:
-    WebDriverWait(ff, 10).until(until)
+    WebDriverWait(ff, 10).until(lambda driver : driver.find_element_by_id("myDynamicElement"))
 finally:
     ff.quit()
