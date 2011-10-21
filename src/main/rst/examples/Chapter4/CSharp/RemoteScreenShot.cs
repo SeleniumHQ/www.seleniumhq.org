5ad01aa113e09219d155d035da9d3a2a69a698ad
Luke Semerau <luke.semerau@gmail.com>
2011-10-21 04:39:14 +0000

    LukeIS: adding C# example for RemoteWebDriver ITakesScreenshot as provided by jimevans on the mailing list 4 months ago... yeah, i'm all over it ;)

--- src/main/rst/examples/Chapter4/CSharp/RemoteScreenShot.cs
+++ src/main/rst/examples/Chapter4/CSharp/RemoteScreenShot.cs
@@ -1,1 +1,19 @@
-TODO
+// Add this class to your code and use this instead of RemoteWebDriver
+// You will then be able to cast it to ITakesScreenshot and call GetScreenshot
+
+public class ScreenShotRemoteWebDriver : RemoteWebDriver, ITakesScreenshot
+{
+       /// <summary>
+       /// Gets a <see cref="Screenshot"/> object representing the image of the page on the screen.
+       /// </summary>
+       /// <returns>A <see cref="Screenshot"/> object containing the image.</returns>
+       public Screenshot GetScreenshot()
+       {
+           // Get the screenshot as base64.
+           Response screenshotResponse = this.Execute(DriverCommand.Screenshot, null);
+           string base64 = screenshotResponse.Value.ToString();
+
+           // ... and convert it.
+           return new Screenshot(base64);
+       }
+}
