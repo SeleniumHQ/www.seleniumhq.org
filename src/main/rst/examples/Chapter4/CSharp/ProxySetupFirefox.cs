517b79a216e75663204e563e0526aa1b27c9cb81
Luke Semerau <luke.semerau@gmail.com>
2011-10-24 17:21:22 +0000

    LukeIS with help from ChrisCulver: adding C# examples for setting proxy on browser startup for IE and FF

--- src/main/rst/examples/Chapter4/CSharp/ProxySetupFirefox.cs
+++ src/main/rst/examples/Chapter4/CSharp/ProxySetupFirefox.cs
@@ -1,1 +1,24 @@
-TODO
+using System;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ProxySetup {
+
+    public class ProxySetup {
+
+        public void SetupProxy() {
+
+            string PROXY = "localhost:8080";
+
+            OpenQA.Selenium.Proxy proxy = new OpenQA.Selenium.Proxy();
+            proxy.HttpProxy = PROXY;
+            proxy.FtpProxy = PROXY;
+            proxy.SslProxy = PROXY;
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability(CapabilityType.Proxy, proxy);
+
+            IWebDriver driver = new FirefoxDriver(cap);
+        }
+    }
+}
--- src/main/rst/examples/Chapter4/CSharp/ProxySetupIE.cs
+++ src/main/rst/examples/Chapter4/CSharp/ProxySetupIE.cs
@@ -1,1 +1,23 @@
-TODO
+using System;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+
+namespace ProxySetup {
+
+    public class ProxySetup {
+
+        public void SetupProxy() {
+
+            string PROXY = "localhost:8080";
+
+            OpenQA.Selenium.Proxy proxy = new OpenQA.Selenium.Proxy();
+            proxy.HttpProxy = PROXY;
+            proxy.FtpProxy = PROXY;
+            proxy.SslProxy = PROXY;
+            DesiredCapabilities cap = DesiredCapabilities.InternetExplorer();
+            cap.SetCapability(CapabilityType.Proxy, proxy);
+
+            IWebDriver driver = new RemoteWebDriverDriver(new Uri("http://localhost:4444/wd/hub"), cap);
+        }
+    }
+}
