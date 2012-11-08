9bce05ee18dfb0598fff2ae21d865cbff1dc79e6
Luke Semerau <luke.semerau@gmail.com>
2012-11-08 17:36:14 +0000

    LukeIS: updating sehq webdriver python example to use the new expected_conditions and also removing an unused import. Fixes Issue 4100

--- src/main/rst/examples/Chapter3/Python/Selenium2Example.py
+++ src/main/rst/examples/Chapter3/Python/Selenium2Example.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
-import time
+from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
@@ -23,7 +23,7 @@ print driver.title
 
 try:
     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
+    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
 
     # You should see "cheese! - Google Search"
     print driver.title
