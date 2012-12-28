9e56b911fba75ba567076e9a443a47c9b96c6c45
Jason Lebya <jmleyba@gmail.com>
2012-12-28 20:23:05 +0000

    JasonLeyba: Clean-up WebDriverJS example for SeHQ

--- src/main/rst/examples/Chapter3/Javascript/Selenium2Example.js
+++ src/main/rst/examples/Chapter3/Javascript/Selenium2Example.js
@@ -1,20 +1,22 @@
 var driver = new webdriver.Builder().build();
-driver.get('http://www.google.com').then(function() {
-  return driver.findElement(webdriver.By.name('q')).then(function(searchBox){
-    return searchBox.sendKeys('webdriver').then(function() {
-      return driver.findElement(webdriver.By.name('btnG')).then(function(submitButton) {
-        return submitButton.click().then(function() {
-          var newTitle;
-          driver.wait(function() {
-            driver.getTitle().then(function(title) {
-              newTitle = title;
-            });
-            return (newTitle && newTitle != "Google");
-          }, 3000).then(function () {
-            require('assert').equal('webdriver - Google Search', newTitle);
-          });
-        });
-      });
-    });
-  });
+driver.get('http://www.google.com');
+
+var element = driver.findElement(webdriver.By.name('q'));
+element.sendKeys('Cheese!');
+element.submit();
+
+driver.getTitle().then(function(title) {
+  console.log('Page title is: ' + title);
 });
+
+driver.wait(function() {
+  return driver.getTitle().then(function(title) {
+    return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
+  });
+}, 3000);
+
+driver.getTitle().then(function(title) {
+  console.log('Page title is: ' + title);
+});
+
+driver.quit();
