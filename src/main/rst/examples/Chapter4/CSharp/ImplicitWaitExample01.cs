e8cf90a7111d13fa0bc7977097bdc9f49d662764
rogerdc <roger@rogerdcook.com>
2017-05-24 10:09:31 -0500

    Update for deprecation
    driver.Manage().Timeouts().ImplicitlyWait(<TimeSpan>) is deprecated, replacing with driver.Manage().Timeouts().ImplicitWait = <TimeSpan>

--- src/main/rst/examples/Chapter4/CSharp/ImplicitWaitExample01.cs
+++ src/main/rst/examples/Chapter4/CSharp/ImplicitWaitExample01.cs
@@ -1,6 +1,6 @@
 using (IWebDriver driver = new FirefoxDriver())
 {
-    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
     driver.Url = "http://somedomain/url_that_delays_loading";
     IWebElement myDynamicElement = driver.FindElement(By.Id("someDynamicElement"));
 }
