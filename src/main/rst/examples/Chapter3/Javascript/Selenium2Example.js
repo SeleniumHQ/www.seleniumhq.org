1b5bcf8cd476810985f62286681999817a91cfc3
macroking <ambravaneswaran@gmail.com>
2017-10-22 10:05:51 -0500

    fix: Javascript example chapter 3

--- src/main/rst/examples/Chapter3/Javascript/Selenium2Example.js
+++ src/main/rst/examples/Chapter3/Javascript/Selenium2Example.js
@@ -1,4 +1,6 @@
-var driver = new webdriver.Builder().build();
+var webdriver = require('selenium-webdriver');
+//Make sure geckodriver.exe is availabe in PATH
+var driver = new webdriver.Builder().forBrowser('firefox').build();
 driver.get('http://www.google.com');
 
 var element = driver.findElement(webdriver.By.name('q'));
@@ -6,17 +8,17 @@ element.sendKeys('Cheese!');
 element.submit();
 
 driver.getTitle().then(function(title) {
-  console.log('Page title is: ' + title);
+    console.log('Page title is: ' + title);
 });
 
 driver.wait(function() {
-  return driver.getTitle().then(function(title) {
-    return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
-  });
+    return driver.getTitle().then(function(title) {
+        return title.toLowerCase().lastIndexOf('cheese!', 0) === 0;
+    });
 }, 3000);
 
 driver.getTitle().then(function(title) {
-  console.log('Page title is: ' + title);
+    console.log('Page title is: ' + title);
 });
 
 driver.quit();
